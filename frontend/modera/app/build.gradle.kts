ded33534a6ec5b5b7e241f49ade8a8964dba6630
chrin105 <chrin105@naver.com>
2026-07-22 16:49:11 +0900

    fix: Material 3 의존성 추가
    #S15P11D207-8

--- frontend/modera/app/build.gradle.kts
+++ frontend/modera/app/build.gradle.kts
@@ -51,10 +51,12 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.compose.adaptive)
     implementation(projects.core.common)
     implementation(projects.core.designsystem)
     implementation(projects.core.data)
     implementation(projects.core.model)
+    implementation(projects.core.navigation)
 
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.navigation3.ui)
@@ -63,6 +65,14 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtimeCompose)
     implementation(libs.androidx.lifecycle.viewModel.navigation3)
     implementation(libs.kotlinx.serialization.json)
+
+    // material3
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive)
+    implementation(libs.androidx.compose.material3.adaptive.layout)
+    implementation(libs.androidx.compose.material3.adaptive.navigation)
+    implementation(libs.androidx.compose.material3.adaptive.navigation3)
+    implementation(libs.androidx.compose.material3.windowSizeClass)
 
     ksp(libs.hilt.compiler)
 

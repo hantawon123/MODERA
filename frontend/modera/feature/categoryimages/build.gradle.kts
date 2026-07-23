438974a6e3829d8d280d11c3f38a566019803828
김채린 <chrin105@naver.com>
2026-07-24 00:44:17 +0900

    feat: navigation 연결
    #S15P11D207-13

--- frontend/modera/feature/categoryimages/build.gradle.kts
+++ frontend/modera/feature/categoryimages/build.gradle.kts
@@ -1,13 +1,15 @@
 plugins {
     id("modera.android.feature")
     id("modera.android.hilt")
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
     namespace = "com.ssafy.modera.feature.categoryimages"
 }
 
-dependencies{
+dependencies {
+    implementation(projects.feature.imagedetail)
     implementation(libs.coil.kt)
     implementation(libs.coil.kt.compose)
 }

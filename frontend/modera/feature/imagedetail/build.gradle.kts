0a7c0b5d64bebcb367f68def550a34e03b5f9c26
김채린 <chrin105@naver.com>
2026-07-24 00:36:21 +0900

    add: image detail모듈 생성
    #S15P11D207-13

---
+++ frontend/modera/feature/imagedetail/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("modera.android.feature")
+    id("modera.android.hilt")
+    alias(libs.plugins.kotlinx.serialization)
+}
+
+android {
+    namespace = "com.ssafy.modera.feature.imagedetail"
+}
+
+dependencies {
+    implementation(projects.core.model)
+    implementation(libs.coil.kt)
+    implementation(libs.coil.kt.compose)
+}

da78c0bd93960cfc5231b69e22a3abdc64c8aa40
chrin105 <chrin105@naver.com>
2026-07-30 17:32:12 +0900

    feat: navigation 연결
    #S15P11D207-187

--- frontend/modera/feature/category/src/main/java/com/ssafy/modera/feature/category/navigation/CategoryEntryProvider.kt
+++ frontend/modera/feature/category/src/main/java/com/ssafy/modera/feature/category/navigation/CategoryEntryProvider.kt
@@ -3,14 +3,23 @@ package com.ssafy.modera.feature.category.navigation
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import com.ssafy.modera.feature.category.CategoryRoute
+import com.ssafy.modera.feature.category.search.CategorySearchRoute
 
 fun EntryProviderScope<NavKey>.categoryEntry(
+    onBackClick: () -> Unit,
     onSearchIconClick: () -> Unit,
     onItemClick: (Int) -> Unit,
 ) {
     entry<CategoryNavKey> {
         CategoryRoute(
             onSearchIconClick = onSearchIconClick,
+            onItemClick = onItemClick,
+        )
+    }
+
+    entry<CategorySearchNavKey> {
+        CategorySearchRoute(
+            onBackClick = onBackClick,
             onItemClick = onItemClick,
         )
     }

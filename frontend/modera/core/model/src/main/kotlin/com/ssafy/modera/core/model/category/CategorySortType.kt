1b3149a6bdc2344e08612d69fa430dcefe29b5ef
chrin105 <chrin105@naver.com>
2026-07-30 17:09:50 +0900

    mod: 정렬 enum 수정
    #S15P11D207-187

--- frontend/modera/core/model/src/main/kotlin/com/ssafy/modera/core/model/category/CategorySortType.kt
+++ frontend/modera/core/model/src/main/kotlin/com/ssafy/modera/core/model/category/CategorySortType.kt
@@ -1,9 +1,11 @@
 package com.ssafy.modera.core.model.category
 
 enum class CategorySortType(
+    val label: String,
     val queryValue: String,
 ) {
-    NAME_ASC("name,asc"),
-    UPDATED_AT_DESC("updatedAt,desc"),
-    IMAGE_COUNT_DESC("imageCount,desc"),
+    NAME_ASC(label = "이름순", queryValue = "name,asc"),
+    UPDATED_AT_ASC(label = "최신 순", queryValue = "updatedAt,asc"),
+    UPDATED_AT_DESC(label = "오래된 순", queryValue = "updatedAt,desc"),
+    IMAGE_COUNT_DESC(label = "사진 많은 순", queryValue = "imageCount,desc"),
 }

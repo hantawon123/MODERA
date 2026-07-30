9800a8c1d54ba408442173206c2affbe6a1354db
chrin105 <chrin105@naver.com>
2026-07-30 12:56:55 +0900

    add: category 모듈 추가
    #S15P11D207-187

---
+++ frontend/modera/feature/category/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("modera.android.feature")
+    id("modera.android.hilt")
+    alias(libs.plugins.kotlinx.serialization)
+}
+
+android {
+    namespace = "com.ssafy.modera.feature.category"
+}
+
+dependencies {
+    implementation(libs.androidx.foundation.layout)
+    implementation(projects.feature.categoryimages)
+    implementation(libs.coil.kt)
+    implementation(libs.coil.kt.compose)
+    implementation(libs.androidx.activity.compose)
+}

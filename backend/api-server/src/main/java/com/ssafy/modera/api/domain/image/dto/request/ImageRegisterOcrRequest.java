4be131a68083c7ee9c6b87172526c1d5b17f1eff
hantawon123 <hantawon@naver.com>
2026-07-31 14:11:17 +0900

    chore : test api

--- backend/api-server/src/main/java/com/ssafy/modera/api/domain/image/dto/request/ImageRegisterOcrRequest.java
+++ backend/api-server/src/main/java/com/ssafy/modera/api/domain/image/dto/request/ImageRegisterOcrRequest.java
@@ -1,10 +1,9 @@
 package com.ssafy.modera.api.domain.image.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 
 public record ImageRegisterOcrRequest(
         @Schema(description = "클라이언트 OCR 원문")
-        @NotBlank String rawText
+        String rawText
 ) {
 }

a0e2535a5ab67fead932cfd01cd1e21fc96da8f8
chrin105 <chrin105@naver.com>
2026-07-31 16:44:17 +0900

    feat: 실제 캘린더 어플과 연동
    #S15P11D207-247

--- frontend/modera/feature/calendar/build.gradle.kts
+++ frontend/modera/feature/calendar/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("modera.android.feature")
+    id("modera.android.hilt")
     alias(libs.plugins.kotlinx.serialization)
 }
 
@@ -9,4 +10,5 @@ android {
 
 dependencies {
     implementation(projects.core.designsystem)
+    implementation(libs.androidx.activity.compose)
 }

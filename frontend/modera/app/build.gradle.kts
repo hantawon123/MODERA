73311b976e1e3e72538943e985ff4777b5e67d80
chrin105 <chrin105@naver.com>
2026-07-31 12:44:48 +0900

    feat: navigation 연결
    #S15P11D207-247

--- frontend/modera/app/build.gradle.kts
+++ frontend/modera/app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(projects.feature.home)
     implementation(projects.feature.category)
     implementation(projects.feature.favorite)
+    implementation(projects.feature.calendar)
     implementation(projects.feature.categoryimages)
     implementation(projects.feature.analyzedimagedetail)
     implementation(projects.feature.imageviewer)
--- frontend/modera/feature/home/src/main/kotlin/com/ssafy/modera/feature/home/navigation/HomeEntryProvider.kt
+++ frontend/modera/feature/home/src/main/kotlin/com/ssafy/modera/feature/home/navigation/HomeEntryProvider.kt
@@ -3,17 +3,16 @@ package com.ssafy.modera.feature.home.navigation
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import com.ssafy.modera.core.model.category.Category
-import com.ssafy.modera.core.navigation.Navigator
 import com.ssafy.modera.feature.home.HomeRoute
 
 fun EntryProviderScope<NavKey>.homeEntry(
-    navigator: Navigator,
     onCategoryClick: (Category) -> Unit,
+    onCalendarClick: () -> Unit,
 ) {
     entry<HomeNavKey> {
         HomeRoute(
             onCategoryClick = onCategoryClick,
-            onCalendarClick = { /* TODO: 추후 네비게이션 연결 */ },
+            onCalendarClick = onCalendarClick,
             onSettingsClick = { /* TODO: 추후 네비게이션 연결 */ },
             onSearchResultClick = { /* TODO: 추후 네비게이션 연결 */ },
         )

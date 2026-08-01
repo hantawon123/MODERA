719597f4b190f9786f7e6b6531e08548a9c35af8
김채린 <chrin105@naver.com>
2026-08-02 00:06:28 +0900

    fix: 카테고리 탭 viewModel 구현
    #S15P11D207-189

---
+++ frontend/modera/feature/category/src/main/java/com/ssafy/modera/feature/category/CategoryUiState.kt
@@ -0,0 +1,22 @@
+package com.ssafy.modera.feature.category
+
+import com.ssafy.modera.core.model.analyzedimage.AnalyzedImage
+import com.ssafy.modera.core.model.category.CategorySheetItem
+import com.ssafy.modera.core.model.category.CategorySortType
+
+sealed interface CategoryUiState {
+
+    data object Loading : CategoryUiState
+
+    data object Error : CategoryUiState
+
+    data class Success(
+        val selectedCategoryId: Long,
+        val selectedCategoryTitle: String,
+        val categories: List<CategorySheetItem>,
+        val analyzedImages: List<AnalyzedImage>,
+        val selectedSortType: CategorySortType,
+        val showCategorySheet: Boolean = false,
+        val showSortPopup: Boolean = false,
+    ) : CategoryUiState
+}

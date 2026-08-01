7aa20498ee348f7bb394b890f86e0a424f3de091
김채린 <chrin105@naver.com>
2026-08-01 16:58:38 +0900

    feat: 자연어 검색 api repository 구현
    #S15P11D207-225

--- frontend/modera/core/data/src/main/kotlin/com/ssafy/modera/core/data/di/DataModule.kt
+++ frontend/modera/core/data/src/main/kotlin/com/ssafy/modera/core/data/di/DataModule.kt
@@ -8,6 +8,8 @@ import com.ssafy.modera.core.data.repository.DefaultImageRepository
 import com.ssafy.modera.core.data.repository.ImageRepository
 import com.ssafy.modera.core.data.repository.calendar.DefaultDeviceCalendarRepository
 import com.ssafy.modera.core.data.repository.calendar.DeviceCalendarRepository
+import com.ssafy.modera.core.data.repository.search.DefaultSearchRepository
+import com.ssafy.modera.core.data.repository.search.SearchRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -31,6 +33,11 @@ abstract class DataModule {
     abstract fun bindsAnalyzedImageRepository(
         impl: DefaultAnalyzedImageRepository,
     ): AnalyzedImageRepository
+
+    @Binds
+    abstract fun bindsSearchRepository(
+        impl: DefaultSearchRepository,
+    ): SearchRepository
 
     @Binds
     abstract fun bindsDeviceCalendarRepository(

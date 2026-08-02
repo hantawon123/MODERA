3a0f3a002dfeb6a2a585a83cd1d58039922f734f
minguk <jlkim909@naver.com>
2026-08-02 22:23:46 +0900

    feat: DocumentDetailUiState 추가
    - Loading, Success, Error 상태를 정의하는 sealed interface 구현
    - Success 상태에서 DocumentDetail 데이터를 포함하도록 구성
    
    #S15P11D207-174:

---
+++ frontend/modera/feature/documentdetail/src/main/kotlin/com/ssafy/modera/feature/documentdetail/DocumentDetailUiState.kt
@@ -0,0 +1,16 @@
+package com.ssafy.modera.feature.documentdetail
+
+import com.ssafy.modera.core.model.DocumentDetail
+
+sealed interface DocumentDetailUiState {
+
+    data object Loading : DocumentDetailUiState
+
+    data class Success(
+        val document: DocumentDetail,
+    ) : DocumentDetailUiState
+
+    data class Error(
+        val exception: Throwable,
+    ) : DocumentDetailUiState
+}

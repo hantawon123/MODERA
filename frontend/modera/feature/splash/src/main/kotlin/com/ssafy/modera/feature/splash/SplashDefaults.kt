82b07f885edf5868a9240ac331cf7c5c3db36a13
chrin105 <chrin105@naver.com>
2026-08-03 15:03:12 +0900

    delete: gif 파일 및 관련 코드
    #S15P11D207-292

--- frontend/modera/feature/splash/src/main/kotlin/com/ssafy/modera/feature/splash/SplashDefaults.kt
+++
@@ -1,8 +0,0 @@
-package com.ssafy.modera.feature.splash
-
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
-
-internal object SplashDefaults {
-    val duration: Duration = 6.seconds
-}

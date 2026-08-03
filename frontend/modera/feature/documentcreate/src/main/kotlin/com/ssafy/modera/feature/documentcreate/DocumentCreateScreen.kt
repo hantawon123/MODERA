22f5528aa0a6614cd28e2f7480f657e231b4d2d7
minguk <jlkim909@naver.com>
2026-08-03 15:29:36 +0900

    mod: DocumentCreatingScreen 컴포넌트 core:ui 모듈로 이동
    - DocumentCreatingScreen을 feature:documentcreate에서 core:ui로 이동하여 공통 컴포넌트화
    - 외부 모듈 접근을 위해 컴포넌트 가시성을 internal에서 public으로 변경
    - 관련 문자열 리소스 및 캐릭터 이미지(img_character_document_creating.png)를 core:ui 모듈로 이전
    - 아이템 전환 애니메이션 지속 시간(DocumentCreatingItemDurationMillis)을 1.5초로 조정
    
    #S15P11D207-194:

--- frontend/modera/feature/documentcreate/src/main/kotlin/com/ssafy/modera/feature/documentcreate/DocumentCreateScreen.kt
+++ frontend/modera/feature/documentcreate/src/main/kotlin/com/ssafy/modera/feature/documentcreate/DocumentCreateScreen.kt
@@ -40,8 +40,8 @@ import com.ssafy.modera.core.designsystem.component.Text
 import com.ssafy.modera.core.designsystem.icon.ModeraIcons
 import com.ssafy.modera.core.designsystem.theme.ModeraTheme
 import com.ssafy.modera.core.model.analyzedimage.AnalyzedImage
+import com.ssafy.modera.core.ui.DocumentCreatingScreen
 import com.ssafy.modera.core.ui.ErrorScreen
-import com.ssafy.modera.feature.documentcreate.component.DocumentCreatingScreen
 import com.ssafy.modera.feature.documentcreate.component.SelectedImagesSection
 
 @Composable

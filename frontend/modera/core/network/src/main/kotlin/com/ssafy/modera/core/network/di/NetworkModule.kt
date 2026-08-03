6af600299751e3e7eb135de3c25f13dbb57074b5
minguk <jlkim909@naver.com>
2026-08-03 09:57:00 +0900

    merge: pulled develop/android - NetworkModule의 OkHttpClient 빌더에 180초 readTimeout 적용

--- frontend/modera/core/network/src/main/kotlin/com/ssafy/modera/core/network/di/NetworkModule.kt
+++ frontend/modera/core/network/src/main/kotlin/com/ssafy/modera/core/network/di/NetworkModule.kt
@@ -25,6 +25,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -43,6 +44,10 @@ internal object NetworkModule {
     fun provideOkHttpClient(): OkHttpClient =
         OkHttpClient.Builder()
             .addInterceptor(AccessTokenInterceptor())
+            .readTimeout(
+                180,
+                TimeUnit.SECONDS,
+            )
             .apply {
                 if (BuildConfig.DEBUG) {
                     addNetworkInterceptor(

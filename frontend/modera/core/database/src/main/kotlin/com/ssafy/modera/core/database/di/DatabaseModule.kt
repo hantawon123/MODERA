0670fde98c3091a4a894572161e6c89dce6c24df
minguk <jlkim909@naver.com>
2026-08-05 00:00:44 +0900

    feat: Room 데이터베이스 및 DAO 설정 추가
    - ModeraDatabase 정의 및 관련 엔티티(DocumentEntity, DocumentImageCrossRef) 추가
    - DaosModule을 통해 DocumentDao를 제공하는 Hilt 모듈 구현
    - DatabaseModule 내 Room 데이터베이스 생성 로직 활성화 및 설정
    - 파일명 및 클래스명 오타 수정 (Mudera -> Modera)
    
    #S15P11D207-327:

--- frontend/modera/core/database/src/main/kotlin/com/ssafy/modera/core/database/di/DatabaseModule.kt
+++ frontend/modera/core/database/src/main/kotlin/com/ssafy/modera/core/database/di/DatabaseModule.kt
@@ -1,25 +1,27 @@
 ﻿package com.ssafy.modera.core.database.di
 
-//import android.content.Context
-//import androidx.room.Room
-//import com.ssafy.modera.core.database.ModeraDatabase
-//import dagger.Module
-//import dagger.Provides
-//import dagger.hilt.InstallIn
-//import dagger.hilt.android.qualifiers.ApplicationContext
-//import dagger.hilt.components.SingletonComponent
-//import javax.inject.Singleton
-//
-//@Module
-//@InstallIn(SingletonComponent::class)
-//internal object DatabaseModule {
-//    @Provides
-//    @Singleton
-//    fun provideModeraDatabase(
-//        @ApplicationContext context: Context
-//    ): ModeraDatabase = Room.databaseBuilder(
-//        context,
-//        ModeraDatabase::class.java,
-//        "modera.db"
-//    ).build()
-//}
+import android.content.Context
+import androidx.room.Room
+import com.ssafy.modera.core.database.ModeraDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun providesModeraDatabase(
+        @ApplicationContext context: Context,
+    ): ModeraDatabase =
+        Room.databaseBuilder(
+            context = context,
+            klass = ModeraDatabase::class.java,
+            name = "modera-database",
+        ).build()
+}

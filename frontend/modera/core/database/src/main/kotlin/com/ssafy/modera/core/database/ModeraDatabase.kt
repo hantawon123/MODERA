6b5460c3776d979f38a0693cf01c7b4b176301dc
minguk <jlkim909@naver.com>
2026-08-05 00:00:44 +0900

    feat: Room 데이터베이스 및 DAO 설정 추가
    - ModeraDatabase 정의 및 관련 엔티티(DocumentEntity, DocumentImageCrossRef) 추가
    - DaosModule을 통해 DocumentDao를 제공하는 Hilt 모듈 구현
    - DatabaseModule 내 Room 데이터베이스 생성 로직 활성화 및 설정
    - 파일명 및 클래스명 오타 수정 (Mudera -> Modera)
    
    #S15P11D207-327:

---
+++ frontend/modera/core/database/src/main/kotlin/com/ssafy/modera/core/database/ModeraDatabase.kt
@@ -0,0 +1,20 @@
+﻿package com.ssafy.modera.core.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.ssafy.modera.core.database.dao.DocumentDao
+import com.ssafy.modera.core.database.model.DocumentEntity
+import com.ssafy.modera.core.database.model.DocumentImageCrossRef
+
+@Database(
+    entities = [
+        DocumentEntity::class,
+        DocumentImageCrossRef::class,
+    ],
+    version = 1,
+    exportSchema = true,
+)
+internal abstract class ModeraDatabase : RoomDatabase() {
+
+    abstract fun documentDao(): DocumentDao
+}

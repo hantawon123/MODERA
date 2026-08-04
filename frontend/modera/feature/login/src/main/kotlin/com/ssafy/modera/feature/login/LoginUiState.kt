637a6a0fcb1f5ca3137efc5d4acc6b99395228e5
chrin105 <chrin105@naver.com>
2026-08-04 09:54:10 +0900

    refactor: 로그인 화면 리팩토링
    #S15P11D207-292

--- frontend/modera/feature/login/src/main/kotlin/com/ssafy/modera/feature/login/LoginUiState.kt
+++ frontend/modera/feature/login/src/main/kotlin/com/ssafy/modera/feature/login/LoginUiState.kt
@@ -3,5 +3,10 @@ package com.ssafy.modera.feature.login
 sealed interface LoginUiState {
     data object Idle : LoginUiState
     data object Loading : LoginUiState
-    data class Error(val message: String) : LoginUiState
+    data class Error(val type: LoginError) : LoginUiState
+}
+
+enum class LoginError {
+    KakaoLoginFailed,
+    ServerLoginFailed,
 }

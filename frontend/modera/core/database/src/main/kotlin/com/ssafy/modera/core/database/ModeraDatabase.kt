fb88ee2a1234c0d6b19a2163c7aa5c65595458ae
minguk <jlkim909@naver.com>
2026-08-05 15:28:03 +0900

    feat: AnalyzedImage 및 Category DAO 추가
    - AnalyzedImageDao: 키워드 검색, 카테고리 필터링, 즐겨찾기 및 일정 등록 상태 업데이트 기능 구현
    - CategoryDao: 카테고리 목록 조회, 카테고리별 이미지 개수 집계 및 신규 카테고리 플래그 관리 기능 구현
    - ModeraDatabase: AnalyzedImageEntity, CategoryEntity 등록 및 StringListConverter 적용
    - DaosModule: 신규 DAO들에 대한 의존성 주입(Provides) 로직 추가
    
    #S15P11D207-328:

--- frontend/modera/core/database/src/main/kotlin/com/ssafy/modera/core/database/ModeraDatabase.kt
+++ frontend/modera/core/database/src/main/kotlin/com/ssafy/modera/core/database/ModeraDatabase.kt
@@ -2,19 +2,32 @@
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.ssafy.modera.core.database.dao.AnalyzedImageDao
+import com.ssafy.modera.core.database.dao.CategoryDao
 import com.ssafy.modera.core.database.dao.DocumentDao
+import com.ssafy.modera.core.database.model.AnalyzedImageEntity
+import com.ssafy.modera.core.database.model.CategoryEntity
 import com.ssafy.modera.core.database.model.DocumentEntity
 import com.ssafy.modera.core.database.model.DocumentImageCrossRef
+import com.ssafy.modera.core.database.util.StringListConverter
 
 @Database(
     entities = [
         DocumentEntity::class,
         DocumentImageCrossRef::class,
+        CategoryEntity::class,
+        AnalyzedImageEntity::class,
     ],
     version = 1,
     exportSchema = true,
 )
+@TypeConverters(
+    StringListConverter::class,
+)
 internal abstract class ModeraDatabase : RoomDatabase() {
 
     abstract fun documentDao(): DocumentDao
+    abstract fun analyzedImageDao(): AnalyzedImageDao
+    abstract fun categoryDao(): CategoryDao
 }

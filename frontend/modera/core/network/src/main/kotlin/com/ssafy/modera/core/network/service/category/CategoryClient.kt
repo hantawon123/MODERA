f691751450ef706e9b2f37247cfb3c9d1fc14488
chrin105 <chrin105@naver.com>
2026-08-05 13:58:40 +0900

    fix: import문 추가
    #S15P11D207-288

--- frontend/modera/core/network/src/main/kotlin/com/ssafy/modera/core/network/service/category/CategoryClient.kt
+++ frontend/modera/core/network/src/main/kotlin/com/ssafy/modera/core/network/service/category/CategoryClient.kt
@@ -3,6 +3,7 @@ package com.ssafy.modera.core.network.service.category
 import com.skydoves.sandwich.getOrThrow
 import com.ssafy.modera.core.model.category.CategorySortType
 import com.ssafy.modera.core.network.model.category.CategoriesResponse
+import javax.inject.Inject
 
 class CategoryClient @Inject constructor(
     private val categoryService: CategoryService,

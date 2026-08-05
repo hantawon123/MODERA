a2b7c2664f2be3396f51a7d08b61ba2fc607b1b5
chrin105 <chrin105@naver.com>
2026-08-05 17:54:18 +0900

    fix: 기존 검색 화면 네비게이션 수정

--- frontend/modera/feature/category/src/main/java/com/ssafy/modera/feature/category/navigation/CategoryEntryProvider.kt
+++ frontend/modera/feature/category/src/main/java/com/ssafy/modera/feature/category/navigation/CategoryEntryProvider.kt
@@ -3,24 +3,13 @@ package com.ssafy.modera.feature.category.navigation
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import com.ssafy.modera.feature.category.CategoryRoute
-import com.ssafy.modera.feature.category.search.CategorySearchRoute
 
 fun EntryProviderScope<NavKey>.categoryEntry(
-    onBackClick: () -> Unit,
-    onSearchIconClick: () -> Unit,
     onItemClick: (Long) -> Unit,
 ) {
     entry<CategoryNavKey> { key ->
         CategoryRoute(
             selectedCategoryId = key.selectedCategoryId,
-            onSearchIconClick = onSearchIconClick,
-            onItemClick = onItemClick,
-        )
-    }
-
-    entry<CategorySearchNavKey> {
-        CategorySearchRoute(
-            onBackClick = onBackClick,
             onItemClick = onItemClick,
         )
     }
